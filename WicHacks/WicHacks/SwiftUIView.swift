1d536bb844bb9247d2c71fe9e59d63ac7f519a70
Alia Ulanbek Kyzy <au4943@g.rit.edu>
2024-03-02 15:03:32 -0500

    Changed background color

---
+++ WicHacks/WicHacks/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  WicHacks
+//
+//  Created by Alia Ulanbek Kyzy on 3/2/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
