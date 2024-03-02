a57bfb061b8bb84292f54a559759478d4b64048b
Alia Ulanbek Kyzy <au4943@g.rit.edu>
2024-03-02 16:12:27 -0500

    added cal

--- WicHacks/WicHacks/HomeController.swift
+++ WicHacks/WicHacks/HomeController.swift
@@ -11,9 +11,32 @@ class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createCalendar()
     }
-
+    
+    func createCalendar() {
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 300),
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
 
 }
 
+extension HomeController: UICalendarViewDelegate {
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return nil
+    }
+}
