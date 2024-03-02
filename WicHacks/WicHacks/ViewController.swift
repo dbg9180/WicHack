8c6d5c9801ff0efbdd6306465368026cb5c01dd1
caseyechin <caseyc611@gmail.com>
2024-03-02 14:29:44 -0500

    Created Project

---
+++ WicHacks/WicHacks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WicHacks
+//
+//  Created by Casey Chin on 3/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
