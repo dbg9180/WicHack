89c713c7842424ec0739f88a05f83e56d00fc20c
caseyechin <caseyc611@gmail.com>
2024-03-03 09:27:32 -0500

    done???

--- WicHacks/WicHacks/GachaController.swift
+++ WicHacks/WicHacks/GachaController.swift
@@ -29,17 +29,21 @@ class GachaController: UIViewController {
 
     @IBAction func gachaPull(_ sender: Any) {
         
-        if p >= 50 {
+        if p >= 20 {
             
+            ErrorMsg.isHidden = true
             image1.isHidden = false
             image1.image = images.randomElement()!
             //        image1.animationImages = animation
             //        image1.animationDuration = 0.5
             //        image1.startAnimating()
-            HomeController.points.points -= 50
+            HomeController.points.points -= 20
+            p -= 20
+            PointsCounter.text = String(p)
             
         } else {
             
+            image1.isHidden = true
             ErrorMsg.isHidden = false
             
         }
